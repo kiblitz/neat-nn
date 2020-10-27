cdbc472852cc64ee153ec5161adeb98f95a033b0
thisistrivial <g.lee13770334@gmail.com>
2020-10-27 18:05:08 -0400

    Added NN class

---
+++ neat/nn.cpp
@@ -0,0 +1,57 @@
+
+#include "nn.h"
+
+NN::NN(std::function<double(double)> activation, 
+       int inputs, 
+       int outputs, 
+       std::uniform_real_distribution<double>& dis, 
+       std::mt19937& gen) {
+  this->activation = activation;
+  this->inputLayer.size = inputs;
+  this->outputLayer.size = outputs;
+  for (int o = 0; o < outputs; ++o) {
+    this->outputLayer.nodes.push_back(o + inputs);
+    this->incoming[o + inputs] = std::vector<int>();
+    for (int i = 0; i < inputs; ++i) {
+      struct Gene gene;
+      gene.innov = i + o * inputs;
+      gene.in = i;
+      gene.out = o + inputs;
+      this->genotype.push_back(gene);
+      this->inputLayer.nodes.push_back(i);
+      this->incoming[o].push_back(i);
+      this->weights[{i, o + inputs}] = dis(gen);
+    }
+  }
+}
+
+double NN::propagateRecurse(std::map<int, double>& memo, const int& node) {
+  std::vector<int> invNeighbors = this->incoming[node];
+  double sum = 0;
+  for (int i = 0; i < invNeighbors.size(); ++i) {
+    int nodeN = invNeighbors[i];
+    if (memo.count(nodeN) == 0) {
+      memo[nodeN] = propagateRecurse(memo, nodeN);
+    }
+    sum += memo[nodeN]; 
+  }
+  return this->activation(sum);
+}
+
+std::vector<double> NN::propagate(const std::vector<double>& input) {
+  if (input.size() != this->inputLayer.size) {
+    throw std::runtime_error("NN input layer size does not match input vector size");
+  }
+
+  std::map<int, double> memo;
+  for (int i = 0; i < this->inputLayer.size; ++i) {
+    int node = this->inputLayer.nodes[i];
+    memo[node] = input[i];
+  }
+  
+  std::vector<double> output;
+  for (int i = 0; i < this->outputLayer.size; ++i) {
+    output.push_back(propagateRecurse(memo, this->outputLayer.nodes[i]));
+  }
+  return output;
+}
---
+++ neat/nn.h
@@ -0,0 +1,44 @@
+
+#include <functional>
+#include <map>
+#include <random>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+struct Gene {
+  int innov;
+  int in;
+  int out;
+  bool enabled = true;
+};
+
+struct Layer {
+  std::vector<int> nodes;
+  size_t size;
+};
+
+class NN {
+  private:
+    std::vector<Gene> genotype; 
+    std::map<std::pair<int, int>, double> weights;
+    Layer inputLayer;
+    Layer outputLayer;
+    std::map<int, std::vector<int>> incoming;
+  
+    std::function<double(double)> activation; 
+
+    double propagateRecurse(std::map<int, double>& memo, const int& node);
+
+    NN addNode();
+    NN addConn();
+  public:
+    NN(std::function<double(double)> activation, 
+       int inputs, 
+       int outputs, 
+       std::uniform_real_distribution<double>& dis, 
+       std::mt19937& gen);
+    std::vector<double> propagate(const std::vector<double>& input);
+    NN mutate();
+    NN breed(const NN& mate);
+};
